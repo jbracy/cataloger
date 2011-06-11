128a8359ea2d6167d757a15f9a52babcd0a61852
James R. Bracy <waratuman@gmail.com>
2011-06-11 17:45:58 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc1'
+gem 'unicorn'
+gem 'capistrano'
+
+
+gem 'haml'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+gem 'authlogic'
+gem 'squeel'
+
+group :development do
+  gem 'wirble'
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'faker'
+  gem 'turn', :require => false
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cataloger::Application.config.secret_token = '67582e6a25a3f901d7b568f4df3ac63604a502e01a5fd0595f81378cb8d6593c10f10b660fc0832c2b8e44fcdd643b05ed3e9b0ae10d48783012bbbcd153df09'
